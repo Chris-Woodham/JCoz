da4f5f46ab40a369f96f718af57dcdf40b595ce3
Chris-Woodham <christopher.woodham@thehutgroup.com>
2022-12-05 17:38:55 +0000

    changes to prevent same method being used in too many experiments - attempt 5

--- src/native/profiler.cc
+++ src/native/profiler.cc
@@ -86,7 +86,7 @@ jmethodID Profiler::mbean_cache_method_id;
 JNIEnv *Profiler::jni_;
 // Adding a hash table to track number of experiments for each method
 // This is in order to prevent JCoz over-sampling common methods (e.g. TokenService:66 was picked approx 20% of times for experiments in previous large run)
-std::unordered_map<long, int> number_method_experiments_hash_table;
+std::unordered_map<jmethodID, int> number_method_experiments_hash_table;
 #define MAX_NO_EXPERIMENTS_PER_METHOD 250
 
 // How long should we wait before starting an experiment
@@ -362,18 +362,18 @@ Profiler::runAgentThread(jvmtiEnv *jvmti_env, JNIEnv *jni_env, void *args)
       for (int i = 0; i < call_frames.size(); i++)
       {
         exp_frame = call_frames.at(i);
-        if (number_method_experiments_hash_table.find((long) exp_frame.method_id) == number_method_experiments_hash_table.end())
+        if (number_method_experiments_hash_table.find(exp_frame.method_id) == number_method_experiments_hash_table.end())
         {
-          number_method_experiments_hash_table[(long) exp_frame.method_id] = 1;
+          number_method_experiments_hash_table[exp_frame.method_id] = 1;
         }
-        int method_experiment_count = number_method_experiments_hash_table.find((long) exp_frame.method_id);
+        int method_experiment_count = number_method_experiments_hash_table.find((exp_frame.method_id);
         if (method_experiment_count <= MAX_NO_EXPERIMENTS_PER_METHOD || (i == call_frames.size() - 1))
         {
           jvmtiError lineNumberError = jvmti->GetLineNumberTable(exp_frame.method_id, &num_entries, &entries);
           if (lineNumberError == JVMTI_ERROR_NONE)
           {
             // logger->info("Profiler::runAgentThread() - Selecting call frame at index {}/{} with methodID {} L{}", i, call_frames.size(), (void *)exp_frame.method_id, exp_frame.lineno);
-            number_method_experiments_hash_table[(long) exp_frame.method_id] = method_experiment_count + 1;
+            number_method_experiments_hash_table[exp_frame.method_id] = method_experiment_count + 1;
             break;
           }
           else
