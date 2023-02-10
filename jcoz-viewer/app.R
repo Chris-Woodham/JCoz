c2e809b6634f1eb1c22adfdb7674e0e4651b144b
Chris-Woodham <christopher.woodham@thehutgroup.com>
2023-02-10 18:11:00 +0000

    reactive function getJcozData working properly - but not yet managed to get undefined number of plots to wrap in mainPanel

--- jcoz-viewer/app.R
+++ jcoz-viewer/app.R
@@ -1,9 +1,9 @@
 ## Set up packages and clear environment ----
 
+library(shiny)
 library(ggplot2)
 library(dplyr)
 rm(list = ls())
-library(shiny)
 
 
 
@@ -18,7 +18,10 @@ ui <- fluidPage(
       fileInput("dataFile", NULL, accept = ".csv")
     ),
     mainPanel(
-      plotOutput("jcozPlot", width = "400px")
+      flowLayout(
+        plotOutput("jcozPlot", width = "400px")
+      )
+      # uiOutput("ui1")
     )
   )
 )
@@ -28,7 +31,25 @@ ui <- fluidPage(
 ## ShinyApp back end (server) ----
 
 server <- function(input, output, session) {
-  tidyUploadedData <- reactive({
+  # output[["ui1"]] <- renderUI({
+  #   req(input$dataFile)
+  #   data <- getJcozData()
+  #   i = 1:length(unique(data()$selectedClassLineNo))
+  #   UI <- paste0("flowLayout(",
+  #                paste0("plotOutput(",
+  #                       "\"plot", i, "\", ",
+  #                       paste0("width = \"400px\""),
+  #                       ")",
+  #                       collapse = ", "),
+  #                ")")
+  #   eval(parse(text = UI))
+  #   print(length(unique(data()$selectedClassLineNo)))
+  #   for (i in 1:length(unique(data()$selectedClassLineNo))) {
+  #     print(i)
+  #   }
+  # })
+  
+  getJcozData <- reactive(function() {
     req(input$dataFile)
     
     fileExtension <- tools::file_ext(input$dataFile$name)
@@ -38,17 +59,19 @@ server <- function(input, output, session) {
     )
     
     # calculate throughput (Number of progress points hit per second)
-    jcozData$throughput = (data_test$progressPointHits / data_test$duration) * 1000000000
+    jcozData$throughput = (jcozData$progressPointHits / jcozData$duration) * 1000000000
     # calculate min and max throughput
     min_throughput = min(jcozData$throughput)
     max_throughput = max(jcozData$throughput)
+    jcozData
   })
   
   output$jcozPlot <- renderPlot({
-    tidyUploadedData()
+    getJcozData()
+    data <- getJcozData()
     # plot one graph for each 
-    for (method in unique(data_test$selectedClassLineNo)) {
-      method_data = dplyr::filter(data_test, selectedClassLineNo == method)
+    for (method in unique(data()$selectedClassLineNo)) {
+      method_data = dplyr::filter(data(), selectedClassLineNo == method)
       print(length(unique(method_data$speedup)))
       print(input$minSampleSize)
       if (nrow(method_data) >= input$minSampleSize && nrow(filter(method_data, speedup == 0)) >= 3) {
@@ -63,6 +86,26 @@ server <- function(input, output, session) {
       }
     }
   })
+  
+  # output$plot2 <- renderPlot({
+  #   data <- getJcozData()
+  #   # plot one graph for each 
+  #   for (method in unique(data()$selectedClassLineNo)) {
+  #     method_data = dplyr::filter(data(), selectedClassLineNo == method)
+  #     print(length(unique(method_data$speedup)))
+  #     print(input$minSampleSize)
+  #     if (nrow(method_data) >= input$minSampleSize && nrow(filter(method_data, speedup == 0)) >= 3) {
+  #       print(
+  #         ggplot() +
+  #           geom_point(data = method_data, aes(x = speedup, y = throughput), size = 2, alpha = 0.3) +
+  #           geom_point(data = method_data %>% dplyr::group_by(speedup) %>% dplyr::summarise(mean_throughput = mean(throughput)), 
+  #                      aes(x = speedup, y = mean_throughput), size = 5, colour = "navyblue") +
+  #           geom_smooth(data = method_data, aes(x = speedup, y = throughput), method = "loess", se = FALSE) +
+  #           theme_classic()
+  #       )
+  #     }
+  #   }
+  # })
 }
 
 
